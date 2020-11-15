b3062bee74097b7e721fbd09584c6bfa30116d5b
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-11-15 22:00:21 +0100

    Post data is ok

---
+++ src/config/mongoose.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const mongoDB = 'mongodb://127.0.0.1/tracer-dev';
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
